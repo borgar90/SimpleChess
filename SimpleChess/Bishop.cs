a2b45a1af355204106c0f1674021ae4184e2fbc1
borgar90 <73884383+borgar90@users.noreply.github.com>
2021-06-08 10:44:42 +0200

    Add project files.

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+{
+    class Bishop :Piece
+    {
+        public Bishop() : base("Bishop")
+        {
+            
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        public Rook() : base("Rook")
+        {
+            
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
